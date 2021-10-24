ebe736536aa8bb99bebbdd4229744875b2d6f5cc
nithinCronj <nithin@cronj.com>
2021-10-24 22:40:07 +0530

    display url

--- testNode/call.js
+++ testNode/call.js
@@ -1,7 +1,7 @@
 var http=require('http');
-var day=require('./sample.js');
+//var day=require('./sample.js');
 http.createServer(function(req, res){
     res.writeHead(200,{'content-Type': 'text/html'});
-    res.write("The date "+day.myDate());
+    res.write(req.url);
     res.end();
 }).listen(8080);
